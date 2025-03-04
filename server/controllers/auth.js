9bd85b6c4c5bdac56f728ce26b05a2478fb1513e
Mohit Aggarwal <mohitkumar06612@gmail.com>
2025-03-05 09:15:57 +1100

    generating the verification token

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,40 @@
 const UserModel = require("../models/users");
 const crypto = require("crypto");
+const VerificationToken = require("../models/verificationTokens");
 
 exports.createNewUser = async (req, res) => {
-  const { email, password, name } = req.body;
+  try {
+    const { email, password, name } = req.body;
 
-  if (!name) return res.status(422).json({ message: "Name is missing" });
-  if (!name) return res.status(422).json({ message: "Email is missing" });
-  if (!password)
-    return res.status(422).json({ message: "Password is missing" });
+    if (!name) return res.status(422).json({ message: "Name is missing" });
+    if (!name) return res.status(422).json({ message: "Email is missing" });
+    if (!password)
+      return res.status(422).json({ message: "Password is missing" });
 
-  const existingUser = await UserModel.findOne({ email });
+    const existingUser = await UserModel.findOne({ email });
 
-  if (existingUser)
-    return res.status(401).json({ message: "Unauthorized request, email is already in use!" });
+    if (existingUser)
+      return res
+        .status(401)
+        .json({ message: "Unauthorized request, email is already in use!" });
 
-  await UserModel.create({ name, email, password });
-  res.send("ok");
+    //new user
+    const newUser = await UserModel.create({ name, email, password });
+
+    // Generate a random verification token
+    const token = crypto.randomBytes(32).toString("hex");
+
+    // Store token in the VerificationToken model
+    await VerificationToken.create({ owner: newUser._id, token });
+    res
+      .status(201)
+      .json({
+        message: "User created successfully! Verification token generated.",
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+  // res.send("ok");
 };
